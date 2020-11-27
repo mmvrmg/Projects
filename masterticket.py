a9be0d0fa3303dbe75c8b37d1439cdf8aa017ffd
mmvrmg <72496146+mmvrmg@users.noreply.github.com>
2020-11-27 06:11:59 +0000

    Add files via upload

--- masterticket.py
+++ masterticket.py
@@ -1,18 +1,41 @@
+import sys
+
 TICKET_PRICE = 10
 
-tickets_remaining = 100 
+def main():
 
-print('there are {} tickets remaing'.format(tickets_remaining))
+  tickets_remaining = 100 
 
-name = input('What is your name?  ')
-tickets_requested = input('How many tickets would you like  ')
+  while tickets_remaining:
 
-def calculate_total(amount_requested):
-  return int(amount_requested) * TICKET_PRICE
+    name = input('What is your name?  ')
+    tickets_requested = input('How many tickets would you like  ')
 
-price = ('The total price of the ticket you requested is {} \n'
-         'Would you like to continue? [yes/no]  '.format(calculate_total(tickets_requested)))
+    try:
+      tickets_requested = int(tickets_requested)
+      if tickets_requested > tickets_remaining:
+        raise ValueError("There are only {} tickets remaining".format(tickets_remaining))
 
-print(price)
+    except ValueError as err:
+      print("We ran into an issue. Please try again {}".format(err))
 
+    else: 
 
+      price = tickets_requested * TICKET_PRICE
+      
+      total = ('The total price of the ticket you requested is £{} \n'.format(price))
+      print(total)
+      choice = input('Would you like to continue? [yes/no]  ')
+
+      if choice == 'yes':
+        print('SOLD!')
+        tickets_remaining -= tickets_requested
+        thanks = print('Thank you for your purchase')
+        print('There are {} tickets left'.format(tickets_remaining))
+      else:
+        exit_quote = print('Thank you anyway, {}!'.format(name))
+        sys.exit()
+
+  print('sorry sold out')
+
+main()
